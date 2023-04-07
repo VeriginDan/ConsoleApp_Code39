a51303da9c56dccc4c0d5bd1d7cd98d275faeb17
Дан Веригин <VeriginDan@outlook.com>
2023-04-07 17:10:23 +0700

    Implementation of idea of interfaces

--- ConsoleApp_Code39/ICodeBarcode.cs
+++ ConsoleApp_Code39/ICodeBarcode.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ConsoleApp_Code39
 {
     internal interface ICodeBarcode
     {
+        string Text { get; set; }
+        Bitmap Image { get; set; }
+        Bitmap GetBarcode(string textToCode);
+        string GetText(Bitmap barcodeToDecode);
     }
 }
