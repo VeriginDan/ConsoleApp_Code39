cf3db431a5a6e7beb6be4cb8690a244fa83320d7
Дан Веригин <VeriginDan@outlook.com>
2023-04-08 19:48:32 +0700

    add comments to barcode second revison of abstract class for barcodes

--- ConsoleApp_Code39/Barcode.cs
+++ ConsoleApp_Code39/Barcode.cs
@@ -5,40 +5,39 @@ using System.Text;
 
 namespace ConsoleApp_Code39
 {
-    internal class Barcode : ICodeBarcode
+    abstract internal class Barcode : ICodeBarcode
     {
+        
         private string _text;
         public string Text 
         { 
-            get => _text; 
-            set => throw new NotImplementedException(); 
+            get => _text;
+            set => _text = isText(value) ? value : "";//Console.WriteLine("Text is not supported to make barcode");
+            // check possibility to code text in barcode. Need to implement Event of Exception here
         }
+        
+        abstract protected bool isText(string textToCheck); // realization may be different for different code systems
+        
         private Bitmap _image;
         public Bitmap Image 
         { 
-            get => _image; 
-            set => throw new NotImplementedException(); 
+            get => _image;
+            set => _image = isImage(value) ? value : null; //Console.WriteLine("File is not supported to decode");
+            // check possibility to decode text in barcode. Need to implement Event of Exception here
+            // need revision here - where and how to check
         }
-
-        public Barcode(string textTocode)
+        abstract protected bool isImage(Bitmap textToCheck); // realization may be different for different code systems
+        public Barcode(string textToCode) //We make new object of class based on text and simultaneously obtain barcode from text
         {
-            Text = textTocode;
+            Text = textToCode;
             Image = GetBarcode(Text);
         }
-        public Barcode(Bitmap imageToDecode) 
+        public Barcode(Bitmap imageToDecode) //We make new object of class based on text and simultaneously obtain barcode from text
         {
             Image = imageToDecode;
             Text = GetText(Image);
         }
-
-        public Bitmap GetBarcode(string textToCode)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetText(Bitmap barcodeToDecode)
-        {
-            throw new NotImplementedException();
-        }
+        protected abstract Bitmap GetBarcode(string textToCode);// realization may be different for different code systems
+        protected abstract string GetText(Bitmap barcodeToDecode);   // realization may be different for different code systems   
     }
 }
